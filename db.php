67e9e22587ce1de024afdb7ebeccd1b245ce34fd
Sadi Ahmedova <sadiahmedova@Sadis-MacBook-Pro.local>
2024-12-10 17:26:47 +0200

    Fixed PDO instantiation

--- db.php
+++ db.php
@@ -10,8 +10,7 @@ try {
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $pdo = new PDO($dsn);
-    
+
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
